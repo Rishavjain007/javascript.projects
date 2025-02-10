941532182a170708251249dfb8e7dd12be48f4c8
Rishavjain007 <jainrishavjain2007@gmail.com>
2025-02-10 11:56:25 +0530

    complite

---
+++ joke generator with api/script.js
@@ -0,0 +1,21 @@
+const peraEl = document.getElementById("joke");
+const btnEl = document.getElementById("btn");
+const URL = "https://api.chucknorris.io/jokes/random"; 
+
+async function getJoke() {
+  try{
+    const response = await fetch(URL)
+    const data = await response.json();
+    return data.value;
+  } catch (error) {
+    throw new Error("failed to fetch joke......");
+  }
+}
+btnEl.addEventListener("click", async() => {
+  try {
+    const joke = await getJoke();
+    peraEl.textContent = joke;
+  } catch (error) {
+    peraEl.textContent = error;
+  }
+});
